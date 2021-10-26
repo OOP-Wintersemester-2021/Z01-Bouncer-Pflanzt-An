a529c3e660a5e08bbae785980284fe7dc90b03c9
Alexander Bazo <alexanderbazo@googlemail.com>
2021-10-26 15:53:02 +0200

    Initial commit

---
+++ src/BouncerPflanztAn.java
@@ -0,0 +1,14 @@
+import de.ur.mi.bouncer.apps.BouncerApp;
+import de.ur.mi.bouncer.apps.BouncerLauncher;
+
+public class BouncerPflanztAn extends BouncerApp {
+
+    @Override
+    public void bounce() {
+        loadMap("Field");
+    }
+
+    public static void main(String[] args) {
+        BouncerLauncher.launch("BouncerPflanztAn");
+    }
+}
